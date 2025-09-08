d5d713495f0ad234e327a6a769745d2294ab335b
PDAC95 <patricio@ac95.ca>
2025-09-08 14:16:09 -0400

    PDAC95/12m - Initial Django project setup with modular structure

---
+++ config/settings/production.py
@@ -0,0 +1,18 @@
+from .base import *
+from decouple import config
+
+DEBUG = False
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
+
+# Database for production (PostgreSQL)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
+    }
+}
